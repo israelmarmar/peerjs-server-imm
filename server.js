c4635ffe349f1cb126cd7edacfb36c5526f4dec7
Israel Marques Martins <email.israel.br@gmail.com>
2021-01-12 19:44:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const { ExpressPeerServer } = require("peer");
 
 const app = express();
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 9000, () => {
   console.log("Your app is listening on port " + listener.address().port);
