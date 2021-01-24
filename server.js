706e27a5809de72a7f02102e20af9af612b5f6ec
israel <email.israel.br@gmail.com>
2021-01-24 13:38:10 -0400

    fix: bugs

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/chunkeds', async function(req, res) {
     Promise.all(responses.map(res => res.text()))
   );
 
-  scripts = scripts.map(script => `/* ${res.url} */ ${script}`)
+  scripts = scripts.map((script, index) => `/* ${chunkeds[index]} */ ${script}`)
   res.json(scripts);
   }else
   res.json([]);
